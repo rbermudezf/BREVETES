30d96f0839d4df51de1a5dd72d0cda0da50afab7
Unknown <ehuamani@inty360.com>
2018-12-06 11:00:41 -0500

    Route de app web

--- brevetes-ui/src/app/app.routes.ts
+++ brevetes-ui/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 const appRoutes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: '**', component: NopagefoundComponent }
+    { path: '**', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'dashboard', component: DashboardComponent }
 ];
 export const APP_ROUTES = RouterModule.forRoot( appRoutes, { useHash: true } );
--- brevetes-ui/src/app/services/shared/sidebar.service.ts
+++ brevetes-ui/src/app/services/shared/sidebar.service.ts
@@ -6,12 +6,23 @@ import { Injectable } from '@angular/core';
 export class SidebarService {
 
   menu: any[] = [
+    // Menú principal
     { titulo: 'Principal',
       icono: 'mdi mdi-gauge',
       submenu: [
         { titulo: 'Dashboard', url: '/dashboard' },
         { titulo: 'ProgressBar', url: '/progress' },
         { titulo: 'Gráficas', url: '/graficas1' }
+      ] },
+
+    // Menú documentos
+    { titulo: 'Documentos',
+      icono: 'mdi mdi-file-document',
+      submenu: [
+        { titulo: 'Registro de trámite', url: '/dashboard' },
+        { titulo: 'Evaluación', url: '/progress' },
+        { titulo: 'Informe', url: '/graficas1' },
+        { titulo: 'Entrega de carnet', url: '/graficas1' }
       ] }
   ];
 
